905144c75db2a9c4a98a3032527646c9e6d2fd06
Paolo Angeli <pangeli70@gmail.com>
2022-10-07 12:34:19 +0200

    Forgot to update mod.ts

--- mod.ts
+++ mod.ts
@@ -6,3 +6,12 @@
 export * from "./src/classes/ApgEdrPublicBinFileResource.ts";
 export * from "./src/classes/ApgEdrPublicTextFileResource.ts";
 export * from "./src/classes/ApgEdrStaticResource.ts";
+export * from "./src/classes/ApgEdrLoggableResource.ts";
+export * from "./src/classes/ApgEdrResource.ts";
+
+export * from "./src/enums/eApgEdrRequestParamSimpleTypes.ts";
+
+export type { IApgEdrLoggableRequest } from "./src/interfaces/IApgEdrLoggableRequest.ts"
+export type { IApgEdrProtectedRequest } from "./src/interfaces/IApgEdrProtectedRequest.ts"
+export type { IApgEdrRequestExt } from "./src/interfaces/IApgEdrRequestExt.ts"
+export type { IApgEdrStaticResourceCacheableItem } from "./src/interfaces/IApgEdrStaticResourceCacheableItem.ts"
